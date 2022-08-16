d360a10f45852828b342f7d9c6b8eb7923afb234
joserot <jose.rotchen14@gmail.com>
2022-08-16 11:47:38 -0300

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,7 @@ import "../styles/index.scss";
 
 export default function MyApp({ Component, pageProps }) {
 	useEffect(() => {
-		import("bootstrap/js/dist/button");
-		import("bootstrap/js/dist/carousel");
-		import("bootstrap/js/dist/dropdown");
+		import("bootstrap/dist/js/bootstrap");
 	}, []);
 	return <Component {...pageProps} />;
 }
