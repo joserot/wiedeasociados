5ad2b8dcf613e5dc220df434f35dcdbc598f9996
joserot <jose.rotchen14@gmail.com>
2022-08-16 15:33:40 -0300

    commit 12

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
-//import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "../styles/index.scss";
 
 export default function MyApp({ Component, pageProps }) {
 	useEffect(() => {
-		import("bootstrap/dist/js/*.{js}");
-		import("bootstrap/js/dist/**/*.{js}");
+		import("bootstrap/dist/js/bootstrap");
 	}, []);
 	return <Component {...pageProps} />;
 }
