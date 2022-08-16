33f7b85011119e1bfb7008ca6c0f7920a2120abb
joserot <jose.rotchen14@gmail.com>
2022-08-16 15:27:49 -0300

    commit 11

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,8 @@ import "../styles/index.scss";
 
 export default function MyApp({ Component, pageProps }) {
 	useEffect(() => {
-		import("bootstrap/dist/js/bootstrap");
+		import("bootstrap/dist/js/*.{js}");
+		import("bootstrap/js/dist/**/*.{js}");
 	}, []);
 	return <Component {...pageProps} />;
 }
