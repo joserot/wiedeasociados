026830813b1a484bd96251f198cd48efab25c3b4
joserot <jose.rotchen14@gmail.com>
2022-08-29 20:39:52 -0300

    add loader nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/index.scss";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }) {
 	const router = useRouter();
@@ -8,7 +10,28 @@ function MyApp({ Component, pageProps }) {
 		typeof document !== undefined
 			? require("bootstrap/dist/js/bootstrap")
 			: null;
-	}, [router.events]);
+	}, [router]);
+
+	useEffect(() => {
+		const handleStart = (url) => {
+			console.log(`Cargando: ${url}`);
+			NProgress.start();
+		};
+
+		const handleStop = () => {
+			NProgress.done();
+		};
+
+		router.events.on("routeChangeStart", handleStart);
+		router.events.on("routeChangeComplete", handleStop);
+		router.events.on("routeChangeError", handleStop);
+
+		return () => {
+			router.events.off("routeChangeStart", handleStart);
+			router.events.off("routeChangeComplete", handleStop);
+			router.events.off("routeChangeError", handleStop);
+		};
+	}, [router]);
 
 	return <Component {...pageProps} />;
 }
